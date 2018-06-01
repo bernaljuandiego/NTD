ab2c6a3311634d16d44ddb0781a4890555b89b16
LeonardoRuiz98 <36464079+LeonardoRuiz98@users.noreply.github.com>
2018-06-01 02:45:33 -0500

    arreglos

--- MarketPlaceKonrad/src/main/webapp/js/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("productos", [
-    'ngRoute',
-    'ui.router',
-    'productoModule'
-]);
--- MarketPlaceKonrad/src/main/webapp/js/producto/producto.module.js
+++
@@ -1,3 +0,0 @@
-var productoModule = angular.module("productoModule", [
-    
-]);
