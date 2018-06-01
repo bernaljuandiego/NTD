1b075fef305295211f4c65b60c5d407cbf9bc6c5
Manuel Steven <mstevennlopez@gmail.com>
2018-05-31 23:16:30 -0500

    CRUD PARA PRODUCTO

--- MarketPlaceKonrad/src/main/webapp/js/routes.js
+++ MarketPlaceKonrad/src/main/webapp/js/routes.js
@@ -1,7 +1,39 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Llamar la aplicacion creada en app.js
+var app = angular.module("productos");
 
-
+app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider){
+    
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider.state("listarProductos",{
+       url: '/producto',
+       //donde se publica
+       views: {
+           mainView:{
+               templateUrl: './js/producto/listar/listarProducto.html',
+               controller: 'listarProductoCtrl'
+           }
+       }
+    });
+    
+    $stateProvider.state("producto",{
+       url:'/producto', 
+       views:{
+           mainView:{
+               templateUrl: './js/producto/crearEditar/crearEditarProducto.html',
+               controller: 'crearEditarProductoCtrl'
+           }
+       }
+    });
+    
+    $stateProvider.state("editarProducto",{
+       url:'/producto/:id', 
+       views:{
+           mainView:{
+               templateUrl: './js/producto/crearEditar/crearEditarProducto.html',
+               controller: 'crearEditarProductoCtrl'
+           }
+       }
+    });
+    
+}]);
